9edf3f6b1957fbb5432cfd1cb6ac563ca9db0673
wjh2012 <wjh2012@naver.com>
2024-04-11 00:12:12 +0900

    user

--- app-domain/src/main/kotlin/com/ggomg/atm/domain/user/User.kt
+++ app-domain/src/main/kotlin/com/ggomg/atm/domain/user/User.kt
@@ -1,8 +1,9 @@
 package com.ggomg.atm.domain.user
 
 data class User(
+    val uid: String,
+    val name: String,
 
-    val username: String
-
-
+    val username: String,
+    val password: String,
 )
